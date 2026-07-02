55b2d01295b6f40082fd6ca414623b46de91b3c7
FloriMecaj <florimecaj31@gmail.com>
2026-07-02 13:43:33 +0200

    VitaLumenNexus

--- src/sanity/lib/load-home-page.ts
+++ src/sanity/lib/load-home-page.ts
@@ -1,9 +1,13 @@
+import { unstable_noStore as noStore } from "next/cache";
+
 import { defaultHomePageContent } from "./default-content";
 import { client } from "./client";
 import { siteSettingsQuery } from "./queries";
 import type { HomePageContent } from "../types";
 
 export async function loadHomePageContent(): Promise<HomePageContent> {
+  noStore();
+
   if (!client) {
     return defaultHomePageContent;
   }
